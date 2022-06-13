5bd3eafb24d3792101d78228e65756997d3eb9b2
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-06-13 14:35:04 +0900

    オプショナルチェイニング 値がない場合もエラーを起こさない

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "ひろ"
+  // hobbies: ["釣り", "キャンプ"]
+};
 
 export const App = () => {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -16,6 +23,7 @@ export const App = () => {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="20px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
